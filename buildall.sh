9deae45eda729867d8b2efba376a55ce929209a7
root <root@CPLROOT.(none)>
2016-02-08 20:24:13 +0800

    修改apr路径

--- buildall.sh
+++ buildall.sh
@@ -3,20 +3,6 @@
 #global setting
 mkdir build
 
-#build apr
-pushd apr-1.5.2
-./configure --prefix=$PWD/../build
-make
-make install
-popd
-
-#build apr-util
-pushd apr-util-1.5.2
-./configure --prefix=$PWD/../build --with-apr=$PWD/../build
-make
-make install
-popd
-
 #build openssl
 pushd openssl-1.0.1r
 ./Configure --prefix=$PWD/../build no-asm no-shared linux-elf
@@ -26,7 +12,7 @@ popd
 
 #build httpd
 pushd httpd-2.4.18
-./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/../build --with-apr-util=$PWD/../build --with-ssl=$PWD/../build --enable-modules=most
+./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-included-apr --with-ssl=$PWD/../build --enable-modules=most
 popd
 
 
