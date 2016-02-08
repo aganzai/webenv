a80ed03cc79215763f2f25c5e116b1ad35b29429
root <root@CPLROOT.(none)>
2016-02-08 17:40:04 +0800

    gitignore文件

--- buildall.sh
+++ buildall.sh
@@ -3,9 +3,30 @@
 #global setting
 mkdir build
 
+#build apr
+pushd apr-1.5.2
+./configure --prefix=$PWD/../build
+make
+make install
+popd
+
+#build apr-util
+pushd apr-util-1.5.2
+./configure --prefix=$PWD/../build --with-apr=$PWD/../build
+make
+make install
+popd
+
+#build openssl
+pushd openssl-1.0.1r
+./Configure --prefix=$PWD/../build no-asm no-shared linux-elf
+make
+make install
+popd
+
 #build httpd
 pushd httpd-2.4.18
-./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=/usr/local/apr --with-apr-util=/usr/local/apr-util --enable-modules=most
+./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/../build --with-apr-util=$PWD/../build --with-ssl=$PWD/../build --enable-modules=most
 popd
 
 
