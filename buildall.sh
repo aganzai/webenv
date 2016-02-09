a0d709598df9441d09e138d0aaff0e10bdfb6f8f
root <root@CPLROOT.(none)>
2016-02-09 12:33:18 +0800

    编译httpd通过

--- buildall.sh
+++ buildall.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 #global setting
-mkdir build
+mkdir mybuild
 
 #build openssl
 pushd openssl-1.0.1r
-./Configure --prefix=$PWD/../build no-asm no-shared linux-elf
+./Configure --prefix=$PWD/../mybuild no-asm no-shared linux-elf
 make
 make install
 popd
 
 #build httpd
 pushd httpd-2.4.18
-./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/srclib/apr --with-apr-util=$PWD/srclib/apr-util --with-included-apr --with-ssl=$PWD/../build --enable-modules=most
+./configure --prefix=$PWD/../mybuild --sysconfdir=$PWD/../mybuild/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/srclib/apr --with-apr-util=$PWD/srclib/apr-util --with-included-apr --with-ssl=$PWD/../mybuild --enable-modules=most
+make
+make install
 popd
 
 
