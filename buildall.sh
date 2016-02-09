873d29537820433df3bdefc2225efc694c70b2b2
root <root@CPLROOT.(none)>
2016-02-09 11:48:45 +0800

    修改buildall

--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #global setting
 mkdir build
@@ -12,7 +12,7 @@ popd
 
 #build httpd
 pushd httpd-2.4.18
-./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-included-apr --with-ssl=$PWD/../build --enable-modules=most
+./configure --prefix=$PWD/../build --sysconfdir=$PWD/../build/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/srclib/apr --with-apr-util=$PWD/srclib/apr-util --with-included-apr --with-ssl=$PWD/../build --enable-modules=most
 popd
 
 
