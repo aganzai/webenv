c841eb9daeb0c6f7b3f617b730b2b182ece9889d
root <root@CPLROOT.(none)>
2016-02-10 20:36:39 +0800

    添加jpeg lib

--- buildall.sh
+++ buildall.sh
@@ -38,9 +38,16 @@ make
 make install
 popd
 
+#build jpeglib
+pushd jpeg-9b
+./configure --prefix=$PWD/../mybuild
+make
+make install
+popd
+
 #build php
 pushd php-5.4.45
-./configure --prefix=$PWD/../mybuild --enable-fpm  --with-mcrypt=/usr/local/libmcrypt --with-zlib --with-zlib-dir=$PWD/../mybuild --with-bz2=$PWD/../mybuild --enable-mbstring --with-openssl-dir=$PWD/../mybuild --with-mysql --with-mysqli --with-mysql-sock --with-gd --with-jpeg-dir=/usr/lib --enable-gd-native-ttf  --enable-pdo --with-pdo-mysql --with-gettext --with-curl=$PWD/../mybuild --with-pdo-mysql --enable-sockets --enable-bcmath --enable-xml --enable-zip --enable-freetype
+./configure --prefix=$PWD/../mybuild --enable-fpm  --with-mcrypt=/usr/local/libmcrypt --with-zlib --with-zlib-dir=$PWD/../mybuild --with-bz2=$PWD/../mybuild --enable-mbstring --with-openssl-dir=$PWD/../mybuild --with-mysql --with-mysqli --with-mysql-sock --with-gd --with-jpeg-dir=$PWD/../mybuild --enable-gd-native-ttf  --enable-pdo --with-pdo-mysql --with-gettext --with-curl=$PWD/../mybuild --with-pdo-mysql --enable-sockets --enable-bcmath --enable-xml --enable-zip --enable-freetype
 make
 make install
 popd
