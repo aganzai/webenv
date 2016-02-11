ca64840994a03dc26b466e15a31669bca19cfbfe
root <root@CPLROOT.(none)>
2016-02-11 09:28:35 +0800

    添加libmcrypt

--- buildall.sh
+++ buildall.sh
@@ -2,17 +2,11 @@
 
 #global setting
 mkdir mybuild
+export LD_LIBRARY_PATH=$PWD/mybuild/lib:$LD_LIBRARY_PATH
 
 #build openssl
 pushd openssl-1.0.1r
 ./Configure --prefix=$PWD/../mybuild no-asm no-shared linux-elf
-make
-make install
-popd
-
-#build httpd
-pushd httpd-2.4.18
-./configure --prefix=$PWD/../mybuild --sysconfdir=$PWD/../mybuild/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/srclib/apr --with-apr-util=$PWD/srclib/apr-util --with-included-apr --with-ssl=$PWD/../mybuild --enable-modules=most
 make
 make install
 popd
@@ -45,9 +39,31 @@ make
 make install
 popd
 
+#build libpng
+pushd libpng-1.6.21
+CPPFLAGS=-I$PWD/../mybuild/include/ LDFLAGS=-L$PWD/../mybuild/lib ./configure --prefix=$PWD/../mybuild
+make
+make install
+popd
+
+#build libmcrypt
+pushd libmcrypt-2.5.7
+./configure --prefix=$PWD/../mybuild
+make
+make install
+popd
+
 #build php
 pushd php-5.4.45
-./configure --prefix=$PWD/../mybuild --enable-fpm  --with-mcrypt=/usr/local/libmcrypt --with-zlib --with-zlib-dir=$PWD/../mybuild --with-bz2=$PWD/../mybuild --enable-mbstring --with-openssl-dir=$PWD/../mybuild --with-mysql --with-mysqli --with-mysql-sock --with-gd --with-jpeg-dir=$PWD/../mybuild --enable-gd-native-ttf  --enable-pdo --with-pdo-mysql --with-gettext --with-curl=$PWD/../mybuild --with-pdo-mysql --enable-sockets --enable-bcmath --enable-xml --enable-zip --enable-freetype
+./configure --prefix=$PWD/../mybuild --enable-fpm  --with-mcrypt=$PWD/../mybuild --with-zlib --with-zlib-dir=$PWD/../mybuild --with-bz2=$PWD/../mybuild --enable-mbstring --with-openssl-dir=$PWD/../mybuild --with-mysql --with-mysqli --with-mysql-sock --with-gd --with-jpeg-dir=$PWD/../mybuild --enable-gd-native-ttf  --enable-pdo --with-pdo-mysql --with-gettext --with-curl=$PWD/../mybuild --with-png-dir=$PWD/../mybuild --with-pdo-mysql --enable-sockets --enable-bcmath --enable-xml --enable-zip --enable-freetype
+make
+make install
+popd
+
+
+#build httpd
+pushd httpd-2.4.18
+./configure --prefix=$PWD/../mybuild --sysconfdir=$PWD/../mybuild/etc/httpd --enable-so --enable-ssl --enable-cgi --enable-rewrite --with-zlib --with-pcre --with-apr=$PWD/srclib/apr --with-apr-util=$PWD/srclib/apr-util --with-included-apr --with-ssl=$PWD/../mybuild --enable-modules=most
 make
 make install
 popd
